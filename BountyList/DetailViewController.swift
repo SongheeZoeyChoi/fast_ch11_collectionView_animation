50e91731e1a9aa46fab6186baa2d839abf1c8229
Songhee Choi <Songhee Choi>
2022-03-06 00:18:04 +0900

    View 속성을 이용한 애니메이션 효과 적용

--- BountyList/DetailViewController.swift
+++ BountyList/DetailViewController.swift
@@ -47,38 +47,73 @@ class DetailViewController: UIViewController {
     }
     
     func prepareAnimation() {
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     func showAnimation() {
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
+        // 1. Constraint를 이용한 애니메이션 적용
+//        nameLabelCenterX.constant = 0
+//        bountyLabelCenterX.constant = 0
+//
+//        // 오른쪽에서 왼쪽으로
+//        UIView.animate(withDuration: 0.3,
+//                       delay: 0.1,
+//                       options: .curveEaseIn,
+//                       animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+//
+//        // +튕기면서(usingSpringWithDamping)
+//        UIView.animate(withDuration: 0.3,
+//                       delay: 0.1,
+//                       usingSpringWithDamping: 0.6,
+//                       initialSpringVelocity: 2,
+//                       options: .allowUserInteraction,
+//                       animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+//
+//        // 이미지 회전 애니메이션
+//        UIView.transition(with: imgView,
+//                          duration: 0.5,
+//                          options: .transitionFlipFromRight,
+//                          animations: nil,
+//                          completion: nil)
         
-        // 오른쪽에서 왼쪽으로
-        UIView.animate(withDuration: 0.3,
-                       delay: 0.1,
-                       options: .curveEaseIn,
-                       animations: {
-            self.view.layoutIfNeeded()
-        }, completion: nil)
-        
-        // +튕기면서(usingSpringWithDamping)
-        UIView.animate(withDuration: 0.3,
-                       delay: 0.1,
+        // 2. UIView속성을 이용한 애니메이션 적용
+        // 1) nameLabel
+        UIView.animate(withDuration: 1,
+                       delay: 0,
                        usingSpringWithDamping: 0.6,
                        initialSpringVelocity: 2,
                        options: .allowUserInteraction,
                        animations: {
-            self.view.layoutIfNeeded()
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
         }, completion: nil)
         
-        // 이미지 회전 애니메이션
+        UIView.animate(withDuration: 1,
+                       delay: 0.2,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 2,
+                       options: .allowUserInteraction,
+                       animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
         UIView.transition(with: imgView,
                           duration: 0.5,
                           options: .transitionFlipFromRight,
                           animations: nil,
                           completion: nil)
+        
     }
     
     func updateUI() {
